34c8f87820e5710094dfe2f4ef22a18dc095a028
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-07 21:22:36 -0500

    Initial Commit

---
+++ Color Sliders/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Color Sliders
+//
+//  Created by Natalie Koenig on 6/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

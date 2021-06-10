6af5509888aa8b213ccac6d77bba3747af453bbf
Natalie Koenig <koenignm@rose-hulman.edu>
2021-06-09 22:42:02 -0500

    Swift code for color changer!

--- Color Sliders/ViewController.swift
+++ Color Sliders/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var alphaValueLabel: UILabel!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("Hello World!")
+        
+        redSlider.value = 0.5
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+        alphaSlider.value = 1.0
+        
+        
     }
 
 
+    @IBAction func sliderChanged(_ sender: Any) {
+        updateView()
+    }
+    
+    func updateView() {
+        let redValue = redSlider.value
+        let greenValue = greenSlider.value
+        let blueValue = blueSlider.value
+        let alphaValue = alphaSlider.value
+        
+        print("Values: \(redValue) \(greenValue) \(blueValue) \(alphaValue)")
+        
+        redValueLabel.text = String(format: "%.2f", redValue)
+        greenValueLabel.text = String(format: "%.2f", greenValue)
+        blueValueLabel.text = String(format: "%.2f", blueValue)
+        alphaValueLabel.text = String(format: "%.2f", alphaValue)
+        
+        colorView.backgroundColor = UIColor.init(red: CGFloat(redValue), green: CGFloat(greenValue), blue: CGFloat(blueValue), alpha: CGFloat(alphaValue))
+    }
 }
 
